8798a4471821bf4a7ba4b79861c9a2816e7188c6
Everton Carneiro <everton.euclides@gmail.com>
2021-09-01 19:22:10 -0300

    Implementado o RecyclerView

--- app/src/main/java/br/com/kotlincontact/ContactAdapter.kt
+++ app/src/main/java/br/com/kotlincontact/ContactAdapter.kt
@@ -1,7 +1,10 @@
 package br.com.kotlincontact
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class ContactAdapter: RecyclerView.Adapter<ContactAdapter.ContactAdapterViewHolder>() {
@@ -9,18 +12,34 @@ class ContactAdapter: RecyclerView.Adapter<ContactAdapter.ContactAdapterViewHold
     private val list: MutableList<Contact> = mutableListOf()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactAdapterViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.contact_item, parent, false)
+        return  ContactAdapterViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ContactAdapterViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bind(list[position])
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return list.size
+    }
+
+    fun updateList(list: List<Contact>) {
+        this.list.clear()
+        this.list.addAll(list)
+        notifyDataSetChanged()
+
     }
 
     class ContactAdapterViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        private val tvName: TextView = itemView.findViewById(R.id.tv_name)
+        private val tvPhone: TextView = itemView.findViewById(R.id.tv_phone)
+        private val ivPhotograph: ImageView = itemView.findViewById(R.id.iv_photograph)
 
+        fun bind(contact: Contact) {
+            tvName.text = contact.name
+            tvPhone.text = contact.phone
+
+        }
     }
 }
--- app/src/main/java/br/com/kotlincontact/MainActivity.kt
+++ app/src/main/java/br/com/kotlincontact/MainActivity.kt
@@ -2,10 +2,71 @@ package br.com.kotlincontact
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private val rvList: RecyclerView by lazy {
+        findViewById<RecyclerView>(R.id.rv_list)
+    }
+
+    private val adapter = ContactAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bindViews()
+        updateList()
+    }
+
+    private fun bindViews() {
+        rvList.adapter = adapter
+        rvList.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun updateList(){
+        adapter.updateList(
+            arrayListOf(
+                Contact(
+                    "Seu Fulano",
+                    "(81) 9999-9999",
+                    "img.png"
+                ),
+                Contact(
+                    "Zé Arrombado",
+                    "(11) 0000-0000",
+                    "img.png"
+                )
+            )
+        )
+    }
+
+    private fun showToast(message: String){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId){
+            R.id.item_menu_1 -> {
+                showToast("Exibindo item de menu 1")
+                true
+            }
+            R.id.item_menu_2 -> {
+                showToast("Exibindo item de menu 2")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
